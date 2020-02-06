ed5f85748a56f8a47e1815cf3b8fa58197c89f96
ellomp <60709145+ellomp@users.noreply.github.com>
2020-02-06 17:27:54 +0200

    Kodutöö esimene commit

---
+++ Homework1-3/Homework 1/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }        //Properties enable a class to expose a public way of getting and setting values
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")] //The Display attribute specifies what to display for the name of a field (in this case "Release Date" instead of "ReleaseDate").
+        [DataType(DataType.Date)] //dataType attributes
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)] //on one line
+        public string Rating { get; set; }
+
+    }
+}
